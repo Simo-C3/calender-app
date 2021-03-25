90224a7cb0f678e71e1371619ecd1b829f156902
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-03-26 01:51:16 +0900

    [add] カレンダーUI

--- redux-sample/src/components/Counter/container.jsx
+++ redux-sample/src/components/Counter/container.jsx
@@ -1,3 +1,32 @@
+import { connect } from "react-redux";
+import { increment, decrement } from "../../redux/count/actions";
+
 import Counter from "./presentation";
 
-export default Counter;
+const mapStateProps = ({ count }) => ({ count });
+/*
+上記と同じ
+const mapStateProps = state => {
+    return { count: state.count };
+}
+*/
+/*
+storeの状態を引数として受け取り必要なものをpropsに流す。
+*/
+
+const mapDispatchProps = dispatch => ({
+    increment: count => {
+        dispatch(increment(count));
+    },
+    decrement: count => {
+        dispatch(decrement(count));
+    }
+});
+/*
+引数にdispatchを受け取り、必要なactionだけをdispatchする関数を定義してpropsとして渡す。
+*/
+
+export default connect(
+    mapStateProps,
+    mapDispatchProps
+)(Counter);
